a486030c54681f706419941f2b2b7a06b4b22d18
Sam Wyness <samwyness@Sams-MacBook-Pro.local>
2024-06-29 23:54:20 +1000

    add homebrew steps to setup and install scriptsjjj

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env zsh
 
+echo "Starting install script..."
+
 pushd $DOTFILES
+echo "Stow files and directories..."
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
     echo "stow linking $folder"
     stow -D $folder
     stow $folder
 done
+echo "Stow complete."
 popd
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+echo "Installing Homebrew bundle..."
+brew bundle --file $DOTFILES/homebrew/Brewfile
--- setup.sh
+++ setup.sh
@@ -1,12 +1,29 @@
-#!/usr/bin/env bash
-
 #!/usr/bin/env zsh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 if [[ -z $STOW_FOLDERS ]]; then
-    STOW_FOLDERS="git,nvim,wezterm,zsh"
+    STOW_FOLDERS="git,homebrew,nvim,wezterm,zsh"
 fi
 
 if [[ -z $DOTFILES ]]; then
     DOTFILES=$HOME/.dotfiles
 fi
 
+# Create Code subdirectories
+echo "Creating dev playground..."
+mkdir $HOME/dev/_playground
+
+echo "Creating dev directory for personal projects..."
+mkdir $HOME/dev/samwyness
+
+# Run installation
 STOW_FOLDERS=$STOW_FOLDERS DOTFILES=$DOTFILES $DOTFILES/install.sh
